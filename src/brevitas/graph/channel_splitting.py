828317f4417b551ca6b61636cbae1f743f199494
Fabian Grob <grobfab@gmail.com>
2024-02-28 03:28:48 -0800

    Feat (channel-splitting): functionalizing splitting mechanism for quant models

--- src/brevitas/graph/channel_splitting.py
+++ src/brevitas/graph/channel_splitting.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import math
-from typing import Dict, List, Set, Tuple, Union
+from typing import Callable, Dict, List, Optional, Set, Tuple, Union
 
 import torch
 import torch.nn as nn
@@ -23,6 +23,16 @@ _conv = (
     nn.Conv1d, nn.Conv2d, nn.Conv3d, nn.ConvTranspose1d, nn.ConvTranspose2d, nn.ConvTranspose3d)
 
 _unsupported_layers = (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d, nn.LayerNorm)
+
+
+# example quant split function
+def quant_split_evenly(channel, scale, zero_point, module):
+    return channel / 2., channel / 2., scale, scale, zero_point, zero_point
+
+
+def quant_duplicate_input(channel, scale, zero_point, module):
+    # no need to return anything else
+    return channel, channel, scale, scale, zero_point, zero_point
 
 
 def _channels_to_split(
@@ -60,12 +70,132 @@ def _channels_to_split(
 
 # decorator is needed to modify the weights in-place using a view
 @torch.no_grad()
-def _split_channels(
+def _split_quantized_channels(
+        module: nn.Module, channels_to_split: torch.Tensor, split_input: bool,
+        split_func: Callable) -> None:
+    """
+    Given a QuantModule, this method splits the weight channels and scales in case of per_channel
+    quantization. It differs from _split_quantized_channels as the actual splitting of channels and scales
+    might needs access to the quantization methods and parameters.
+    """
+    weight = module.weight.data
+    bias = module.bias.data if module.bias is not None else None
+    num_added_channels = len(channels_to_split)
+
+    _get_axis = _get_input_axis if split_input else _get_output_axis
+    axis = _get_axis(module)
+    # save shape of the module weights
+    orig_shape = list(weight.shape)
+    weight_t = transpose(weight, axis)
+    # flatten to 2d
+    weight_t = weight_t.reshape(weight_t.size(0), -1)
+
+    # check for per_channel quantization
+    is_per_channel_quant = False
+    is_asym_quant = False
+    # we can only split scales etc. for output channels, so check if we are splitting output
+    if not split_input and module.weight_quant.scale().shape[axis] == orig_shape[axis]:
+        is_per_channel_quant = True
+        scales = module.weight_quant.tensor_quant.scaling_impl.value.data
+        orig_scales_shape = scales.shape
+        # get view of scales
+        scales_t = transpose(scales, axis)
+        # flatten to 2d
+        scales_t = scales_t.reshape(scales_t.size(0), -1)
+        try:
+            # get zero_points, for sym quantization the zero point is 1D
+            if module.weight_quant.tensor_quant.zero_point_impl.value.shape == orig_scales_shape:
+                is_asym_quant = True
+                zero_points = module.weight_quant.tensor_quant.zero_point_impl.value.data
+                # get view of scales
+                zero_points_t = transpose(zero_points, axis)
+                # flatten to 2d
+                zero_points_t = zero_points_t.reshape(zero_points_t.size(0), -1)
+        except AttributeError:
+            # nothing to do with the zero points, that's 0 anyways
+            pass
+    for id in channels_to_split:
+        # get channel to split
+        channel_to_split = weight_t[id, :]
+        # get scale for channel
+        scale_to_split = scales_t[id] if not split_input else module.weight_quant.scale()
+        # get zero point
+        zero_point_to_split = zero_points[id] if is_asym_quant else torch.tensor(0.)
+        # split channel/scale/zero_point based on user defined method, i.e. halfing it, duplicating it
+        split_values = split_func(channel_to_split, scale_to_split, zero_point_to_split, module)
+        assert len(split_values) == 6, 'split method needs to return 6 values: 2x channel, 2x scale, 2x zero_point'
+        # unpack all split_values
+        split_channel_0, split_channel_1, split_scale_0, split_scale_1, zero_point_0, zero_point_1 = split_values
+        # set original channel to split_channel_0 and then stack the second channel
+        weight_t[id, :] = split_channel_0
+        # expand so we can stack
+        split_channel_1 = split_channel_1.expand(1, split_channel_1.size(0))
+        weight_t = torch.cat([weight_t, split_channel_1], dim=0)
+
+        # if per_channel quant, we need to create a new scale for the added channel
+        if is_per_channel_quant:
+            scales_t[id] = split_scale_0
+            split_scale_1 = split_scale_1.unsqueeze(0)
+            # stacking the newly created scale
+            scales_t = torch.cat([scales_t, split_scale_1], dim=0)
+
+        # zero points in case of asym
+        if is_asym_quant:
+            zero_points[id] = zero_point_0
+            zero_point_1 = zero_point_1.unsqueeze(0)
+            zero_points = torch.cat([zero_points, zero_point_1], dim=0)
+
+        if bias is not None and not split_input:
+            # TODO:
+            # currently hardcoded to be split evenly, should bias be included in split_func?
+            bias[id] *= 0.5
+            split_bias = bias[id:id + 1]
+            bias = torch.cat((bias, split_bias))
+
+    # reshape weight_t back to orig shape with the added channels
+    del orig_shape[axis]
+    weight_t = weight_t.reshape(weight_t.size(0), *orig_shape)
+    weight_t = transpose(weight_t, axis)
+    module.weight.data = weight_t
+
+    if bias is not None:
+        module.bias.data = bias
+
+    if is_per_channel_quant:
+        # we don't care about the axis dim, we need all other dim for reshaping later
+        orig_scales_shape = list(orig_scales_shape)
+        del orig_scales_shape[axis]
+        # reshape scale back to orig shape, with adapted dim for split scales
+        scales_t = scales_t.reshape(scales_t.size(0), *orig_scales_shape)
+        scales_t = transpose(scales_t, axis)
+        # set value for scaling_impl to scales
+        module.weight_quant.tensor_quant.scaling_impl.value.data = scales_t
+
+    if is_asym_quant:
+        # set zero_points to module, reshape using scales shape as it's the same
+        zero_points_t = zero_points_t.reshape(zero_points_t.size(0), *orig_scales_shape)
+        zero_points_t = transpose(zero_points_t, axis)
+        module.weight_quant.tensor_quant.zero_point_impl.value.data = zero_points_t
+
+    if isinstance(module, _conv):
+        if split_input:
+            module.in_channels += num_added_channels
+        else:
+            module.out_channels += num_added_channels
+    elif isinstance(module, nn.Linear):
+        if split_input:
+            module.in_features += num_added_channels
+        else:
+            module.out_features += num_added_channels
+
+
+# decorator is needed to modify the weights in-place using a view
+@torch.no_grad()
+def _split_unquantized_channels(
         module: nn.Module,
         channels_to_split: torch.Tensor,
         split_input: bool = False,
-        split_factor: float = 0.5,
-        use_quant_error: bool = False) -> None:
+        split_factor: float = 0.5) -> None:
     """
     Given a module, this method splits the weight channels as proposed in https://arxiv.org/abs/1901.09504.
     `split_factor` determines how to split the channels, `channels_to_split` is a list of channel indices.
@@ -82,83 +212,26 @@ def _split_channels(
     weight_t = transpose(weight, axis)
     # flatten to 2d
     weight_t = weight_t.reshape(weight_t.size(0), -1)
-
-    # check for quantized layer and get scales if per channel quant
-    is_per_channel_quant = False
-    is_asym_quant = False
-    if isinstance(module,
-                  QuantLayerMixin) and module.weight_quant.scale().shape[axis] == orig_shape[axis]:
-        is_per_channel_quant = True
-        orig_scales_shape = module.weight_quant.tensor_quant.scaling_impl.value.shape
-        # get view of scales
-        scales = module.weight_quant.tensor_quant.scaling_impl.value.data.flatten()
-        # get zero_points, for sym quantization the zero point is 1D
-        try:
-            if module.weight_quant.tensor_quant.zero_point_impl.value.shape == orig_scales_shape:
-                is_asym_quant = True
-                zero_points = module.weight_quant.tensor_quant.zero_point_impl.value.data.flatten()
-        except AttributeError:
-            # nothing to do with the zero points, that's 0 anyways
-            print('sym quant')
     for id in channels_to_split:
-        # just adding a new channel with the quantization error
-        if isinstance(module, QuantLayerMixin) and use_quant_error:
-            # adding a new channel with the quantization error
-            # get channel to split and expand that channel for concatenating
-            split_channel = weight_t[id, :]
-            # get scale, zero_point, and bit width
-            scale_for_split = scales[id]
-            zero_point_for_split = zero_points[id] if is_asym_quant else 0.
-            bit_width = module.weight_quant.bit_width()
-            split_channel = split_channel - module.weight_quant.tensor_quant.int_quant(
-                scale_for_split, zero_point_for_split, bit_width, split_channel)
-        else:
-            # split and get channel to stack
-            weight_t[id, :] *= split_factor
-            split_channel = weight_t[id, :]
+        # split and get channel to stack
+        weight_t[id, :] *= split_factor
+        split_channel = weight_t[id, :]
         # expand so we can stack
         split_channel = split_channel.expand(1, split_channel.size(0))
         weight_t = torch.cat([weight_t, split_channel], dim=0)
 
         if bias is not None and not split_input:
             bias[id] *= split_factor
-            split_bias = bias[id:id + 1]
-            bias = torch.cat((bias, split_bias))
-
-        if is_per_channel_quant:
-            # TODO: add option to keep it or half it or set it to 0
-            scales[id] *= split_factor
-            split_scale = scales[id:id + 1]
-            scales = torch.cat([scales, split_scale], dim=0)
-
-        if is_asym_quant:
-            # duplicate zero_point, no point of splitting it
-            zero_point_channel = zero_points[id:id + 1]
-            zero_points = torch.cat([zero_points, zero_point_channel], dim=0)
+            split_channel = bias[id:id + 1]
+            bias = torch.cat((bias, split_channel))
 
     # reshape weight_t back to orig shape with the added channels
     del orig_shape[axis]
     weight_t = weight_t.reshape(weight_t.size(0), *orig_shape)
     weight_t = transpose(weight_t, axis)
     module.weight.data = weight_t
-
     if bias is not None:
         module.bias.data = bias
-
-    if is_per_channel_quant:
-        # TODO: add option to initialize scale for the new channel differently
-        # we don't care about the axis dim, we need all other dim for reshaping later
-        orig_scales_shape = list(orig_scales_shape)
-        del orig_scales_shape[axis]
-        # reshape scale back to orig shape, with adapted dim for split scales
-        scales = scales.reshape(-1, *orig_scales_shape)
-        # set value for scaling_impl to scales
-        module.weight_quant.tensor_quant.scaling_impl.value.data = scales
-
-    if is_asym_quant:
-        # set zero_points to module
-        zero_points = zero_points.reshape(-1, *orig_scales_shape)  # same shape as scales
-        module.weight_quant.tensor_quant.zero_point_impl.value.data = zero_points
 
     if isinstance(module, _conv):
         if split_input:
@@ -172,29 +245,55 @@ def _split_channels(
             module.out_features += num_added_channels
 
 
+def _duplicate_channels(
+        module, channels_to_split, duplicate_input, quant_duplicating_func=quant_duplicate_input):
+    # wrapper to simply use duplicating functions
+    if isinstance(module, QuantLayerMixin):
+        # duplicate using duplicate func
+        _split_quantized_channels(
+            module,
+            channels_to_split,
+            split_input=duplicate_input,
+            split_func=quant_duplicating_func)
+    else:
+        # duplicate the channels as before
+        _split_unquantized_channels(
+            module, channels_to_split, split_input=duplicate_input, split_factor=1)
+
+
+def _split_channels(module, channels_to_split, split_input, quant_split_func=quant_split_evenly):
+    # wrapper for splitting channels in quant/unquant modules
+    if isinstance(module, QuantLayerMixin):
+        # split quantized channels using the specified splitting mechanism
+        _split_quantized_channels(
+            module, channels_to_split, split_input, split_func=quant_split_func)
+    else:
+        # split channels regularly
+        _split_unquantized_channels(module, channels_to_split, split_input, split_factor=0.5)
+
+
 def _split_channels_region(
         sources: Dict[str, nn.Module],
         sinks: Dict[str, nn.Module],
         channels_to_split: torch.tensor,
         split_input: bool,
-        use_quant_error: bool) -> None:
+        quant_split_func: Callable = quant_split_evenly) -> None:
     if not split_input:
         # splitting output channels
         for module in sources:
             _split_channels(
-                module, channels_to_split, split_input=False, use_quant_error=use_quant_error)
+                module, channels_to_split, split_input=False, quant_split_func=quant_split_func)
         for module in sinks:
             # duplicating input_channels for all modules in the sink
-            _split_channels(module, channels_to_split, split_factor=1, split_input=True)
+            _duplicate_channels(module, channels_to_split, duplicate_input=True)
     else:
         # input channels are split in half, output channels duplicated
         for module in sinks:
             _split_channels(
-                module, channels_to_split, split_input=True, use_quant_error=use_quant_error)
-
+                module, channels_to_split, split_input=True, quant_split_func=quant_split_func)
         for module in sources:
             # duplicating output_channels for all modules in the source
-            _split_channels(module, channels_to_split, split_factor=1, split_input=False)
+            _duplicate_channels(module, channels_to_split, duplicate_input=False)
 
 
 def _is_groupwise(module: nn.Module) -> bool:
@@ -251,8 +350,8 @@ def _split(
         regions: List[Region],
         split_ratio: float,
         split_input: bool,
-        split_criterion: str = 'maxabs',
-        use_quant_error: bool = False) -> GraphModule:
+        quant_split_func: Optional[Callable] = quant_split_evenly,
+        split_criterion: str = 'maxabs') -> GraphModule:
     for i, region in enumerate(regions):
         sources = [region.get_module_from_name(src) for src in region.srcs_names]
         sinks = [region.get_module_from_name(sink) for sink in region.sinks_names]
@@ -274,7 +373,7 @@ def _split(
             sinks=sinks,
             channels_to_split=channels_to_split,
             split_input=split_input,
-            use_quant_error=use_quant_error)
+            quant_split_func=quant_split_func)
 
     return model
 
@@ -323,13 +422,13 @@ class GraphChannelSplitting(GraphTransform):
             split_ratio: float = 0.02,
             split_criterion: str = 'maxabs',
             split_input: bool = True,
-            use_quant_error: bool = False):
+            quant_split_func: Optional[Callable] = quant_split_evenly):
         super(GraphChannelSplitting, self).__init__()
 
         self.split_ratio = split_ratio
         self.split_criterion = split_criterion
         self.split_input = split_input
-        self.use_quant_error = use_quant_error
+        self.quant_split_func = quant_split_func
 
     def apply(
             self,
@@ -345,7 +444,7 @@ class GraphChannelSplitting(GraphTransform):
                 split_ratio=self.split_ratio,
                 split_criterion=self.split_criterion,
                 split_input=self.split_input,
-                use_quant_error=self.use_quant_error)
+                quant_split_func=self.quant_split_func)
         if return_regions:
             return model, regions
         else:
