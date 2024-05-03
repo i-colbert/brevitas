2be83f270b4de233a83949ccc20fbc16c90c1567
i-colbert <Ian.Colbert@amd.com>
2024-05-03 09:01:59 -0700

    Update eval.py

--- src/brevitas_examples/llm/llm_quant/eval.py
+++ src/brevitas_examples/llm/llm_quant/eval.py
@@ -41,7 +41,7 @@ def model_eval(model, valenc, seqlen):
         for inps in valenc:
             lm_logits = model(**inps)['logits']
             shift_logits = lm_logits[:, :-1, :].contiguous()
-            shift_labels = inps['input_ids'][:, 1:].to(device)
+            shift_labels = inps['input_ids'][:, 1:].to(model.device)
             loss_fct = nn.CrossEntropyLoss()
             loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
             neg_log_likelihood = loss.float() * seqlen
